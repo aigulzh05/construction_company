072045f25b13a93bf897869e36714ff43d131ef9
Bekzhan Satiev <tarantuldeveloper@gmail.com>
2023-04-24 12:48:25 +0600

    refresh token repository

---
+++ server/src/main/java/com/horizonbuilders/server/repository/RefreshTokenRepository.java
@@ -0,0 +1,15 @@
+package com.horizonbuilders.server.repository;
+
+import com.horizonbuilders.server.model.RefreshToken;
+import com.horizonbuilders.server.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Integer> {
+    Optional<RefreshToken> findByUser(User user);
+
+    boolean existsByUser(User user);
+}
