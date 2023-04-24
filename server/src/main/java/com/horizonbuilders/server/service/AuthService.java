01979b0a105c2fb1dd80fc0b2d0b7a5c8be7b6d7
Bekzhan Satiev <tarantuldeveloper@gmail.com>
2023-04-24 12:46:42 +0600

    generate refresh token service

--- server/src/main/java/com/horizonbuilders/server/service/AuthService.java
+++ server/src/main/java/com/horizonbuilders/server/service/AuthService.java
@@ -1,4 +1,7 @@
 package com.horizonbuilders.server.service;
 
+import com.horizonbuilders.server.dto.LoginResponse;
+
 public interface AuthService {
+    LoginResponse authenticate(String username, String password);
 }
