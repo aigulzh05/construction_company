dd3c75ec2777e1446bb06f51fc7287b53f0ec142
Bekzhan Satiev <tarantuldeveloper@gmail.com>
2023-04-24 13:24:26 +0600

    add /api

--- server/src/main/java/com/horizonbuilders/server/controller/AuthController.java
+++ server/src/main/java/com/horizonbuilders/server/controller/AuthController.java
@@ -2,19 +2,17 @@ package com.horizonbuilders.server.controller;
 
 import com.horizonbuilders.server.dto.LoginRequest;
 import com.horizonbuilders.server.dto.LoginResponse;
-import com.horizonbuilders.server.jwt.JwtUtils;
 import com.horizonbuilders.server.service.AuthService;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 @RequiredArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class AuthController {
