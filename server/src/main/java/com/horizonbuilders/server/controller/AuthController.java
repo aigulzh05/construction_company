59f8e51937af95186fbaa27cc2e91d0bdf52ebc0
Bekzhan Satiev <tarantuldeveloper@gmail.com>
2023-04-24 19:05:18 +0600

    refresh token endpoint

--- server/src/main/java/com/horizonbuilders/server/controller/AuthController.java
+++ server/src/main/java/com/horizonbuilders/server/controller/AuthController.java
@@ -1,10 +1,14 @@
 package com.horizonbuilders.server.controller;
 
-import com.horizonbuilders.server.dto.LoginRequest;
-import com.horizonbuilders.server.dto.LoginResponse;
+import com.horizonbuilders.server.dto.request.LoginRequest;
+import com.horizonbuilders.server.dto.request.RefreshAccessTokenRequest;
+import com.horizonbuilders.server.dto.response.LoginResponse;
 import com.horizonbuilders.server.service.AuthService;
+import com.horizonbuilders.server.service.RefreshTokenService;
+import jakarta.validation.Valid;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
+import lombok.Value;
 import lombok.experimental.FieldDefaults;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,9 +22,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
 
     final AuthService authService;
+    final RefreshTokenService refreshTokenService;
 
     @PostMapping("/authenticate")
     public LoginResponse authenticate(@RequestBody LoginRequest loginRequest) {
         return authService.authenticate(loginRequest.username(), loginRequest.password());
+    }
+
+    @PostMapping("/refresh-token")
+    public LoginResponse refreshAccessTokenByRefreshToken(@Valid @RequestBody RefreshAccessTokenRequest request) {
+        return refreshTokenService.generateAccessTokenByRefreshToken(request);
     }
 }
